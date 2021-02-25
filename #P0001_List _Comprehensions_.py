31be4873232d17068956941df92ef8c76230248f
Ujjwal Omar <ujjwalomar0714@gmail.com>
2021-02-25 20:29:53 +0530

    Update #P0001_List _Comprehensions_.py

--- #P0001_List _Comprehensions_.py
+++ #P0001_List _Comprehensions_.py
@@ -1,9 +1,9 @@
 # list comprehensions
 
-# list comprehensions make the code cleaner as well works faster than normal loops.
+# list comprehensions make the code cleaner as well works faster than normal looping.
 
 # ----- ----- -----
-l=[i for i in range(10)]  # integer list from 0 to 100.
+l=[i for i in range(10)]  # integer list from 0 to 10.
 
 # this is equivalent to:
 l1=[]
@@ -11,7 +11,7 @@ for i in range(10):
     l1.append(i)
     
 # ----- ----- -----
-m=[i**2 for i in range(10)]  # list of square of integers from 1 to 100.
+m=[i**2 for i in range(10)]  # list of square of integers from 1 to 10.
 
 # this is equivalent to:
 m1=[]
@@ -20,7 +20,7 @@ for i in range(10):
     
 # ----- ----- -----
 n=[print(1) for i in range(10)]  # print loop comprehended in a list.
-
+# executing line 22 returns n=[None, None, None, None, None, None, None, None, None, None]
 # this is equivalent to:
 for i in range(10):
     print(1)
