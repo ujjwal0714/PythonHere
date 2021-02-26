58913aa489037da55945390b28e2928bad947cbd
Ujjwal Omar <ujjwalomar0714@gmail.com>
2021-02-26 16:59:01 +0530

    Update #M0002.py

--- Python & Maths/#M0002.py
+++ Python & Maths/#M0002.py
@@ -1,6 +1,16 @@
-# recursive calculation of factorial.
+# recursive and iterative calculation of factorial.
 
-def f(x):
+def f(x):  # recursive
   if x==0:
     return(1)
   return(x*f(x-1))
+
+def g(x):  # iterative
+  if x>=0:
+    p=1
+  else:
+    return(False)  # Factorial Not Defined
+  while x>0:
+    p*=x
+    x-=1
+  return(p)
